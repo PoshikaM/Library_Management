5c81f1fabd111f11c2b3fa0d78dde23e62835940
PoshikaM <poshika.m@kalvium.community>
2025-02-23 13:59:34 +0530

    Initialised backend with PostgreSQL on Neon

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`PORT ${PORT} is working fine`)
+})
